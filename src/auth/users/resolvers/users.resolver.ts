eadacee5a9037c2e60159a961ea7857cab2b9406
Jose E. Martinez <>
2022-06-27 09:56:55 -0600

    +added docs of how to use

--- src/auth/users/resolvers/users.resolver.ts
+++ src/auth/users/resolvers/users.resolver.ts
@@ -1,19 +1,27 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { StringSchemaDefinition } from 'mongoose';
 import { UsersService } from '../services/users.service';
 import { CreateUserInput } from '../users.input';
 import { User } from '../users.schema';
 
 @Resolver(() => User)
 export class UsersResolver {
-    constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService) {}
 
-    @Mutation(() => User)
-    async createUser(@Args('input') input: CreateUserInput) {
-      return this.usersService.create(input);
-    }
-  
-    @Query(() => [User])
-    async users() {
-      return this.usersService.find();
-    }
+  @Mutation(() => User)
+  async createUser(@Args('input') input: CreateUserInput) {
+    return this.usersService.create(input);
+  }
+
+  @Query(() => [User])
+  async users() {
+    return this.usersService.find();
+  }
+
+  @Query(() => User)
+  async user(
+    @Args('email', { type: () => String, nullable: false }) email: string,
+  ) {
+    return await this.usersService.findOne(email);
+  }
 }
--- src/auth/users/services/users.service.ts
+++ src/auth/users/services/users.service.ts
@@ -6,21 +6,22 @@ import { Injectable, Inject } from '@nestjs/common';
 import { User } from '../users.schema';
 
 @Injectable()
-export class UsersService { 
-    constructor(
-        @Inject('USER_MODEL')
-        private userModel: Model<User>,
-      ) {}
-    
-      async create(input: FilterQuery<User>): Promise<User> {
-        return this.userModel.create(input);
-      }
-    
-      async findOne(query: FilterQuery<User>): Promise<User> {
-        return this.userModel.findOne(query).lean();
-      }
-    
-      async find(): Promise<User[]> {
-        return this.userModel.find().lean();
-      }
+export class UsersService {
+  constructor(
+    @Inject('USER_MODEL')
+    private userModel: Model<User>,
+  ) {}
+
+  async create(input: FilterQuery<User>): Promise<User> {
+    return this.userModel.create(input);
+  }
+
+  async findOne(email: string): Promise<User> {
+    const searchQuery = { email };
+    return this.userModel.findOne(searchQuery).lean();
+  }
+
+  async find(): Promise<User[]> {
+    return this.userModel.find().lean();
+  }
 }
